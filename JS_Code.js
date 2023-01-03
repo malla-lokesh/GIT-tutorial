bffefb887dcdfe76616de96fdf7c0ec4efc90dea
malla-lokesh <121810307046@gitam.in>
2023-01-03 22:35:18 +0530

    added Comments for JS code assignment

---
+++ JS_Code.js
@@ -0,0 +1,6 @@
+var ele = document.getElementById("header-title");
+ele.style.borderBottom = 'solid 3px black';
+// Created new list element
+var list = document.getElementsByTagName("li");
+// Changing style for 5th list element.
+list[4].style.color = 'green';
