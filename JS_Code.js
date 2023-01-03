b188de64e558894b88921d424b1122c45ab2f371
malla-lokesh <121810307046@gitam.in>
2023-01-03 22:16:49 +0530

    Added comments to the JS file

--- JS_Code.js
+++ JS_Code.js
@@ -1,4 +1,5 @@
 var ele = document.getElementById("header-title");
 ele.style.borderBottom = 'solid 3px black';
 var list = document.getElementsByTagName("li");
+// Changing style for 5th list element.
 list[4].style.color = 'green';
