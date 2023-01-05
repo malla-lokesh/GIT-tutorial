f1cf87f326d17bd7d024fe14b53a26586b8aff15
malla-lokesh <121810307046@gitam.in>
2023-01-05 20:05:31 +0530

    Deleting data from Local Storage

---
+++ Deleting_data.js
@@ -0,0 +1,31 @@
+var form = document.getElementById('my-form');
+var list = document.getElementById('users');
+
+form.addEventListener('submit', store);
+list.addEventListener('click', remove);
+
+function store(e){
+    e.preventDefault();
+
+    let obj = { 
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value
+            };
+    let obj_serialized = JSON.stringify(obj);
+    localStorage.setItem('myObj', obj_serialized);
+    console.log(JSON.parse(localStorage.getItem('myObj')));
+
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(obj.name + " - " + obj.email));
+    list.appendChild(li);
+
+    var deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deleteBtn);
+}
+
+function remove(e){
+    e.preventDefault();
+    var item = e.target.parentElement;
+    list.removeChild(item);
+}
