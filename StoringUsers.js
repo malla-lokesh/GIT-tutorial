5a14f3cb5d7c6ff77352625c1ba44cdf577d7336
malla-lokesh <121810307046@gitam.in>
2023-01-05 19:30:47 +0530

    Displaying user details

--- StoringUsers.js
+++ StoringUsers.js
@@ -1,14 +1,20 @@
 var form = document.getElementById('my-form');
+var list = document.getElementById('users');
 
 form.addEventListener('submit', store);
 
 function store(e){
     e.preventDefault();
 
-    let obj = { name: document.getElementById('name').value,
+    let obj = { 
+                name: document.getElementById('name').value,
                 email: document.getElementById('email').value
             };
     let obj_serialized = JSON.stringify(obj);
     localStorage.setItem('myObj', obj_serialized);
     console.log(JSON.parse(localStorage.getItem('myObj')));
+
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(obj.name + " - " + obj.email));
+    list.appendChild(li);
 }
